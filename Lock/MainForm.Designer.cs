dd4b530089f3a92effe1f55a16916a72b93934b5
eo39 <zhekaonichsenko@gmail.com>
2019-12-23 10:20:34 +0200

    v0.1
    Добавлен класс полярных координат

--- Lock/MainForm.Designer.cs
+++ Lock/MainForm.Designer.cs
@@ -28,36 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gameField = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gameField)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // gameField
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(624, 529);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.gameField.Location = new System.Drawing.Point(12, 12);
+            this.gameField.Name = "gameField";
+            this.gameField.Size = new System.Drawing.Size(624, 529);
+            this.gameField.TabIndex = 0;
+            this.gameField.TabStop = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(648, 553);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.gameField);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "MainForm";
             this.Text = "Lock Picking";
             this.Load += new System.EventHandler(this.MainForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameField)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox gameField;
     }
 }
 
---
+++ Lock/PolarCoordinates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+
+namespace Lock
+{
+    class PolarCoordinates
+    {
+        private int length;
+
+        public readonly Point StartPosition;
+
+        public Point FinishPosition => new Point((int)(length * Math.Cos(Angle)) + StartPosition.X,
+            (int)(length * Math.Sin(Angle)) + StartPosition.Y);
+
+        public double Angle { get; private set; }
+
+        public PolarCoordinates(int startX, int startY)
+        {
+            StartPosition = new Point(startX, startY);
+            Angle = Math.PI / 2;
+            length = 25;
+        }
+
+        public void TiltStickClockWise()
+        {
+            Angle += Math.PI / 50;
+        }
+
+        public void TiltStickCounterClockWise()
+        {
+            Angle -= Math.PI / 50;
+        }
+    }
+}
